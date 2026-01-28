ef6f98b9926f3297aa540e6c1f8eb5d440c78386
claudiamameli <claudiamameli.work@gmail.com>
2026-01-28 18:39:16 +0100

    Added parts of script for visualisation

--- map_creation.R
+++ map_creation.R
@@ -174,10 +174,11 @@ for(i in names(df_graphs_list)){
 # Load all previosuly saved dataframes
 path_nodes_df <- "~/Desktop/Repositories/MEC8_Snowbed_Alpine_Species/species_specific_df"
 files_nodes_df <- list.files(path_nodes_df, pattern = "\\.csv$", full.names = TRUE)
+df_names <- tools::file_path_sans_ext(basename(files_nodes_df))
 
 
 list2env(setNames(lapply(files_nodes_df, read.csv),
-                  tools::file_path_sans_ext(basename(files_nodes_df))),
+                  df_names),
          envir = .GlobalEnv)
 
 ## 3.1 Line graphs - Summary tables --------------------------------------------
@@ -251,21 +252,81 @@ ggarrange(total_patches_plot, smallest_area_plot, largest_area_plot,
 
 
 ## 3.2 Node visualisation ------------------------------------------------------
-ggplot(data=g_supinum_pres_buff0, 
-       aes(x=longitude, y=latitude, 
-          #size=area_m2
-       )) +
-  geom_point(size = 0.5) +
+# Function for plotting nodes
+plot_nodes <- function(df, scenario, buffer) {
+  ggplot(df, aes(x = longitude, y = latitude)) +
+    geom_point(size = 0.4) +
+    coord_fixed() +
+    theme_bw() +
+    labs(title = paste(scenario, ", Buffer", buffer)) +
+    theme(
+      axis.text  = element_blank(),
+      axis.ticks = element_blank(),
+      plot.title = element_text(size = 16, face = "bold"),
+      legend.position = "none"
+    )
+}
+
+# Singular plots
+plot_nodes(g_supinum_pres_buff0, "Present", "0")
+plot_nodes(g_supinum_pres_buff1, "Present", "1")
+plot_nodes(g_supinum_pres_buff2, "Present", "2")
+plot_nodes(g_supinum_pres_buff3, "Present", "3")
+
+plot_nodes(g_supinum_fut245_buff0, "Future 2-4.5", "0")
+plot_nodes(g_supinum_fut245_buff1, "Future 2-4.5", "1")
+plot_nodes(g_supinum_fut245_buff2, "Future 2-4.5", "2")
+plot_nodes(g_supinum_fut245_buff3, "Future 2-4.5", "3")
+
+plot_nodes(g_supinum_fut585_buff0, "Future 5-8.5", "0")
+plot_nodes(g_supinum_fut585_buff1, "Future 5-8.5", "1")
+plot_nodes(g_supinum_fut585_buff2, "Future 5-8.5", "2")
+plot_nodes(g_supinum_fut585_buff3, "Future 5-8.5", "3")
+
+# To be able to see them all together it is better to create a singular dataframe with all data
+g_supinum_all <- bind_rows(
+  # Present
+  g_supinum_pres_buff0 %>% mutate(scenario = "Present", buffer = "0"),
+  g_supinum_pres_buff1 %>% mutate(scenario = "Present", buffer = "1"),
+  g_supinum_pres_buff2 %>% mutate(scenario = "Present", buffer = "2"),
+  g_supinum_pres_buff3 %>% mutate(scenario = "Present", buffer = "3"),
+  
+  # Future 2–4.5
+  g_supinum_fut245_buff0 %>% mutate(scenario = "Future 2–4.5", buffer = "0"),
+  g_supinum_fut245_buff1 %>% mutate(scenario = "Future 2–4.5", buffer = "1"),
+  g_supinum_fut245_buff2 %>% mutate(scenario = "Future 2–4.5", buffer = "2"),
+  g_supinum_fut245_buff3 %>% mutate(scenario = "Future 2–4.5", buffer = "3"),
+  
+  # Future 5–8.5
+  g_supinum_fut585_buff0 %>% mutate(scenario = "Future 5–8.5", buffer = "0"),
+  g_supinum_fut585_buff1 %>% mutate(scenario = "Future 5–8.5", buffer = "1"),
+  g_supinum_fut585_buff2 %>% mutate(scenario = "Future 5–8.5", buffer = "2"),
+  g_supinum_fut585_buff3 %>% mutate(scenario = "Future 5–8.5", buffer = "3")
+)
+
+# Order scenarios and buffers
+g_supinum_all <- g_supinum_all %>%
+  mutate(scenario = factor(scenario, levels = c("Present", "Future 2–4.5", "Future 5–8.5")),
+    buffer = factor(buffer, levels = c("0", "1", "2", "3")))
+
+
+ggplot(g_supinum_all, aes(x = longitude, y = latitude)) +
+  geom_point(size = 0.4) +
   coord_fixed() +
   theme_bw() +
-  labs(title = "Present, Buffer 0") +
-  theme(#axis.title=element_blank(), 
-        axis.text=element_blank(),
-        axis.ticks=element_blank(),
-        #legend.text = element_text(size = 12),
-        #legend.title = element_text(size = 14, face = "bold"),
-        plot.title = element_text(size = 16, face = "bold"),
-        #axis.title = element_text(size = 14)
-        )
-
-?geom_point
+  facet_grid(
+    rows = vars(scenario),
+    cols = vars(buffer),
+    switch = "y"
+  ) +
+  labs(
+    x = NULL,
+    y = NULL
+  ) +
+  theme(
+    axis.text  = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = "none",
+    strip.text = element_text(size = 14, face = "bold"),
+    strip.background = element_rect(fill = "grey95")
+  )
